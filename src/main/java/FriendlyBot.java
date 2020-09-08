6e33b2d5bc06ffed15a9680bb25745dcd9207efb
Elizabeth Chan <elizabethchan1308@gmail.com>
2020-09-08 14:48:38 +0800

    Level-5: Handle Errors (Non-numeric done input, out of range done input, empty Todo input)

--- src/main/java/FriendlyBot.java
+++ src/main/java/FriendlyBot.java
@@ -20,20 +20,16 @@ public class FriendlyBot {
             String[] checkDone = line.split(" ", 2);
 
             if (checkDone[0].equals("done")) {
-                boolean isInteger = true;
                 try {
                     Integer.parseInt(checkDone[1]);
-                } catch (NumberFormatException nfe) {
-                    isInteger = false;
-                }
-
-                if (isInteger == true) {
                     int d = Integer.parseInt(checkDone[1]);
                     tasks[d - 1].markAsDone();
                     System.out.println("Good job! :) I've marked this task as complete:\n\n\t" +
                             tasks[d - 1].toString() + "\n");
-                } else {
-                    invalidCommand();
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Oops! Please indicate in NUMERALS, which task you've done! 😅\n");
+                } catch (NullPointerException npe) {
+                    System.out.println("Oops! It seems like the task you're trying to set as done is invalid! 🤨\n");
                 }
             } else {
                 if (line.equals("list")) {
@@ -47,26 +43,33 @@ public class FriendlyBot {
                             "See you again! :)\n");
                     break;
                 } else {
-                    String lineFirstWord = line.split(" ")[0];
+                    String lineWords[] = line.split(" ");
 
-                    switch (lineFirstWord) {
+                    switch (lineWords[0]) {
                         case "todo":
-                            tasks[taskCount] = new FriendlyBotTodo(line);
-                            tasks[taskCount].description = line.substring(5);
-                            taskCount = displayNewTask(taskCount, tasks[taskCount]);
+                            try {
+                                checkTodoValidity(lineWords);
+                                tasks[taskCount] = new FriendlyBotTodo(line);
+                                tasks[taskCount].description = line.substring(5);
+                                taskCount = displayNewTask(taskCount, tasks[taskCount]);
+                                break;
+                            }
+                            catch (StringIndexOutOfBoundsException e) {
+                                System.out.println("Oops! You can't leave the description of your Todo empty. 😞");
+                            };
                             break;
                         case "deadline":
-                            int dlBy = line.indexOf("/by") + 4;
-                            String by = line.substring(dlBy);
+                            int deadlineBy = line.indexOf("/by") + 4;
+                            String by = line.substring(deadlineBy);
                             tasks[taskCount] = new FriendlyBotDeadline(line, by);
-                            tasks[taskCount].description = line.substring(9, dlBy - 4);
+                            tasks[taskCount].description = line.substring(9, deadlineBy - 4);
                             taskCount = displayNewTask(taskCount, tasks[taskCount]);
                             break;
                         case "event":
-                            int eAt = line.indexOf("/at") + 4;
-                            String at = line.substring(eAt);
+                            int eventAt = line.indexOf("/at") + 4;
+                            String at = line.substring(eventAt);
                             tasks[taskCount] = new FriendlyBotEvent(line, at);
-                            tasks[taskCount].description = line.substring(6, eAt - 4);
+                            tasks[taskCount].description = line.substring(6, eventAt - 4);
                             taskCount = displayNewTask(taskCount, tasks[taskCount]);
                             break;
                         default:
@@ -79,9 +82,15 @@ public class FriendlyBot {
         }
     }
 
+    public static void checkTodoValidity(String[] lineWords) throws StringIndexOutOfBoundsException {
+        if (lineWords.length==1) {
+            throw new StringIndexOutOfBoundsException();
+        }
+    }
+
     // Refactored from main
     private static void invalidCommand() {
-        System.out.println("You've entered an invalid command :(");
+        System.out.println("Oops! I don't think I know what you mean. ☹");
     }
 
     // Refactored from main
