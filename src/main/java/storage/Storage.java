7236c503f0df569fc0576d3b7c3cc36759cfcfb4
Elizabeth Chan <elizabethchan1308@gmail.com>
2020-09-23 14:25:21 +0800

    Update (JavaDoc)

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,15 +1,30 @@
 package storage;
 
-import bots.FriendlyBot;
-import bots.FriendlyBotTask;
-import bots.FriendlyBotTaskManager;
+import bots.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Stores user's tasks into text file.<br><br>
+ * Specifically, contains readFile and writeFile methods to handle reading from and writing into text file.
+ *
+ * @see Storage#readFile
+ * @see Storage#writeFile(ArrayList, int, File)
+ */
 public class Storage extends FriendlyBot {
 
+    /**
+     * Contains readFile method from FriendlyBotTaskManager.
+     *
+     * @param tasks arraylist containing all of the user's tasks.
+     * @param taskCount to be updated after reading in the tasks in the text file.
+     * @param f text file to read from.
+     * @return taskCount - to update the number of tasks in the user's task list after reading from text file.
+     *
+     * @see FriendlyBotTaskManager#readFile(ArrayList, int, File)
+     */
     // Scanning/Reading info. into text file
     public static int readFile(ArrayList<FriendlyBotTask> tasks, int taskCount, File f) {
         try {
@@ -20,7 +35,16 @@ public class Storage extends FriendlyBot {
         return taskCount;
     }
 
-
+    /**
+     * Contains writeFile method from FriendlyBotTaskManager.
+     *
+     * @param tasks arraylist containing all of the user's tasks.
+     * @param taskCount to keep the task list updated on the current number of tasks before writing into it.
+     * @param f text file to write into.
+     * @return taskCount - to update the number of tasks in the user's task list after writing into the text file.
+     *
+     * @see FriendlyBotTaskManager#writeToFileEvent(StringBuilder, FriendlyBotEvent)
+     */
     // Writing updated info. into text file
     public static int writeFile(ArrayList<FriendlyBotTask> tasks, int taskCount, File f) {
         try {
