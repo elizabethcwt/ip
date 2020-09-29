f3edcc0403f099970726fcb83f5a8f304c4da458
Elizabeth Chan <elizabethchan1308@gmail.com>
2020-09-29 12:48:25 +0800

    Update FriendlyBot.java

--- src/main/java/bots/FriendlyBot.java
+++ src/main/java/bots/FriendlyBot.java
@@ -1,25 +1,11 @@
 package bots;
 
 import storage.Storage;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * <h1>Welcome to FriendlyBot!</h1>
- * <h2>FriendlyBot is a program that helps manage users' task lists.</h2>
- * <h3>Here are FriendlyBot's Features:</h3>
- *
- * <ul>
- *     <li>Stores todos</li>
- *     <li>Stores deadlines with deadline date (Can read yyyy-mm-dd format)</li>
- *     <li>Stores events with event location</li>
- * </ul>
- *
- * @author ElizabethChan
- * @version 1.0
- * @since 2020-08-24
- */
 public class FriendlyBot {
 
     // Creating a new text file to store task list
@@ -28,11 +14,11 @@ public class FriendlyBot {
     private static int taskCount = 0;
 
     /**
-     * Creates a new text file to store users' tasks.<br><br>
-     * Calls the the methods to read and write to the text file, and handles the UI.
+     * Creates a new text file to store users' tasks.
+     * <br><br>Calls the the methods to read and write to the text file, and handles the UI.
      *
      * @throws IOException happens when there is a failure during reading, writing and searching file or directory
-     * operations.
+     *                     operations.
      * @see Storage
      * @see Ui
      */
@@ -64,7 +50,7 @@ public class FriendlyBot {
      * Displays the user's new task.
      *
      * @param taskCount number of tasks in the updated list after the user has entered a new task.
-     * @param task the arraylist containing all the user's tasks thus far.
+     * @param task      the arraylist containing all the user's tasks thus far.
      * @return taskCount - after incrementing it by 1, from adding a new todo to the arraylist.
      */
     public static int displayNewTask(int taskCount, FriendlyBotTask task) {
